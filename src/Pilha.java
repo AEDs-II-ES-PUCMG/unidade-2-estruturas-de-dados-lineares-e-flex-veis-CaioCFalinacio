449ac16c749f2a14eddad30d076ad099a291a583
CaioCFalinacio <caio.falinacio@sga.pucminas.br>
2026-04-29 10:28:03 -0300

    feat: implementação da função subPilha na classe Pilha

--- src/Pilha.java
+++ src/Pilha.java
@@ -61,8 +61,31 @@ public class Pilha<E> {
 	 * @throws IllegalArgumentException se a pilha não contém numItens elementos.
 	 */
 	public Pilha<E> subPilha(int numItens) {
+		Celula<E> aux = topo;
+
+		// Verificando a exceção: numItens é negativo
+		if (numItens < 0) {
+            throw new IllegalArgumentException("O número de itens não pode ser negativo.");
+        }
+
+		// Verificando a exceção: pilha não contém numItens elementos
+		for(int i = 0; i < numItens; i++){
+			if(aux == fundo)
+				throw new IllegalArgumentException("A pilha não contém " + numItens + " elementos.");
+			aux = aux.getProximo();
+		}
 		
-		// TODO
-		return null;
+		Pilha<E> novaPilha = new Pilha<>();
+		Pilha<E> subPilha = new Pilha<>();
+		aux = topo;
+		for(int i = 0; i < numItens; i++){
+			novaPilha.empilhar(aux.getItem());
+			aux = aux.getProximo();
+		}
+		
+		while(!novaPilha.vazia())
+			subPilha.empilhar(novaPilha.desempilhar());
+
+		return subPilha;
 	}
 }
